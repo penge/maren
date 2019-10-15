8b82fe983413e1854eaa6ad29d8ce6b62e4f4afa
Pavel Bucka <ppenge@gmail.com>
2019-10-15 21:10:24 +0000

    Create folders on maren init

--- cli/handlers/init.js
+++ cli/handlers/init.js
@@ -11,7 +11,13 @@ const gitignoreContent = [
 ].join('\n');
 
 /**
- * Initialize <cwd> by creating .gitignore
+ * Initialize <cwd> by creating:
+ *
+ * .gitignore
+ * documents
+ * draft
+ * static
+ * themes
  */
 module.exports = async argv => {
   const { cwd } = argv;
@@ -20,6 +26,11 @@ module.exports = async argv => {
 
   const gitignorePath = path.join(cwd, '.gitignore');
   await fse.writeFile(gitignorePath, gitignoreContent);
+
+  await fse.ensureDir(path.join(cwd, 'documents'));
+  await fse.ensureDir(path.join(cwd, 'draft'));
+  await fse.ensureDir(path.join(cwd, 'static'));
+  await fse.ensureDir(path.join(cwd, 'themes'));
 
   console.log('Done!');
 };
