39c8af8209ecfc8cde3eeed2bfa9665db957e3db
Pavel Bucka <ppenge@gmail.com>
2019-10-18 14:52:09 +0000

    Send "404 Not Found" if 404.html doesn't exist

--- cli/handlers/serve.js
+++ cli/handlers/serve.js
@@ -15,7 +15,11 @@ module.exports = argv => {
   app.use('/', express.static(root));
   app.use('/', express.static(draft));
   app.use((req, res, next) => {
-    res.status(404).sendFile(notFound);
+    res.status(404).sendFile(notFound, err => {
+      if (err) {
+        res.status(404).send('404 Not Found');
+      }
+    });
   });
 
   app.listen(port, () => {
